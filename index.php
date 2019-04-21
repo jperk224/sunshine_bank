0ac94da684169050226505d4bcf73c996f48b069
Jeff <jmperkinson@gmail.com>
2019-04-20 21:06:26 -0400

    added service pics and testimonials

--- index.php
+++ index.php
@@ -210,6 +210,37 @@
                     </div>
                 </div>
             </div>
+            <div class="container-fluid testimonials">
+                <h2>Hear Our Testimonials</h2>
+                <div class="row">
+                    <div class="col-sm">
+                        <div class="info_pics2">
+                            <p><em>“For a small family business like us, we needed to have somebody who was responsive to our needs. Sunshine Bank looked at how they could help us solve problems. That was something we didn’t always find, especially with the larger banks.”</em></p>
+                            <div class="sources">
+                                <p>-Duane Vino, Chair</p>
+                                <p class="company-title">Duane's Vino</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="info_pics2">
+                            <p><em>"Sunshine Bank offers us a small town feel and small bank intimacy with the resources and depth of a large bank."</em></p>
+                            <div class="sources">
+                                <p>-Allison Jones</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="info_pics2">
+                        <p><em>"They really guided us through the documentation process.  They worked hard to get us the coverage we needed to meet our working capital needs."</em></p>
+                            <div class="sources">
+                                <p>-Randy Lacore, President</p>
+                                <p class="company-title">Randy's Beverage Corp.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
